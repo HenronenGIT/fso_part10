77ba1dc09d424408e4577618b6a626f8d2e14f54
Henri Maronen <henri.maronen@slush.org>
2024-02-01 10:43:11 +0200

    ESLint configured

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React-Native let's goo!</Text>
+      <Text>React-Native lets goooo!</Text>
       <StatusBar style="auto" />
     </View>
   );
