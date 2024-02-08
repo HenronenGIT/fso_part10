26d352ede8260741e8e8047e3dd656292bd51f20
Henri Maronen <henrimaronen@gmail.com>
2024-02-08 11:14:24 +0200

    10.11 DONE - useRepositories custom hook created

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from "react";
 import Main from "./src/components/Main";
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "./src/utils/apolloClient";
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <>
       <NativeRouter>
-        <Main />
-        <StatusBar style="auto" />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+          <StatusBar style="auto" />
+        </ApolloProvider>
       </NativeRouter>
     </>
   );
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+import { GET_REPOSITORIES } from "../graphql/queries";
+import { useQuery } from "@apollo/client";
+
+const useRepositories = () => {
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+    // Other options
+  });
+  const repositories = data?.repositories;
+  return { repositories, loading };
+};
+
+export default useRepositories;
