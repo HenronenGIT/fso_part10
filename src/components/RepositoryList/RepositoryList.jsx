d0f1c1439e5bd08d95b87aa15403c7b61e9dce63
Henri Maronen <henrimaronen@gmail.com>
2024-02-16 11:56:12 +0200

    Debouncing effect added for the searchkeyword

--- src/components/RepositoryList/RepositoryList.jsx
+++ src/components/RepositoryList/RepositoryList.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useRepositories from "../../hooks/useRepositories";
 import RepositoryListContainer from "./RepositoryListContainer";
 import { useDebounce } from "use-debounce";
 
 const RepositoryList = () => {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [debouncedSearchKeyword] = useDebounce(searchKeyword, 500);
+
   const [filters, setFilters] = useState({
     orderBy: "CREATED_AT",
     orderDirection: "DESC",
-    searchKeyword: "",
+    searchKeyword: debouncedSearchKeyword,
   });
 
-  const { repositories, loading } = useRepositories(filters);
-
   const updateSearchKeyword = (keyword) => {
+    setSearchKeyword(keyword);
+  };
+
+  useEffect(() => {
     setFilters((currentFilters) => ({
       ...currentFilters,
-      searchKeyword: keyword,
+      searchKeyword: debouncedSearchKeyword,
     }));
-  };
+  }, [debouncedSearchKeyword]);
+
+  const { repositories, loading } = useRepositories(filters);
 
   return (
     <>
